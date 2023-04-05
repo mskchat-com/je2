7de37bb139c7838ab6915e0a618f675fd0bf2be0
jekr <48930765+a7mdal3le@users.noreply.github.com>
2023-04-06 01:44:32 +0300

    Create kb.js

---
+++ kb.js
@@ -0,0 +1,62 @@
+$(`<style>
+.nosel.d-flex.room.r187532ce3daxff0d7ae6-x-9cyice .fitimg.u-pic.borderg {
+  letter-spacing: 1px;
+  background-size: 108% 108%;
+  -webkit-text-fill-color: white;
+  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
+}
+
+.nosel.d-flex.room.r187532ce3daxff0d7ae6-x-9cyice .corner.fa.fa-user.label.uc.fa-microphone.label-danger {
+  letter-spacing: 1px;
+  -webkit-text-fill-color: #000000;
+  border-radius: 15px 0px 15px 0px !important;
+  height: 27px;
+  margin-top: 15px !important;
+  margin-bottom: 0px !important;
+  background-color: #d9534f00;
+  text-align: left !important;
+  margin-left: 50px!important;
+}
+.nosel.d-flex.room.r187532ce3daxff0d7ae6-x-9cyice.bord {border-inline: 0px solid #4c121e !important;}
+.nosel.d-flex.room.r187532ce3daxff0d7ae6-x-9cyice .dots.mini.u-msg {
+  text-align: center;
+  -webkit-text-fill-color: #fff;
+  background-image: url(https://i.ibb.co/bB812zy/61.gif);
+  background-size: 100% 100%;
+  margin-top: 80px !important;
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 6px #fff;
+}
+.nosel.d-flex.room.r187532ce3daxff0d7ae6-x-9cyice {
+    background-image: url(https://up6.cc/2023/02/167581354780311.gif);
+    background-size: cover;
+    border-radius: 15px 0px 15px 0px;
+    display: inline-block;
+}
+.nosel.d-flex.room.r187532ce3daxff0d7ae6-x-9cyice .fitimg.u-pic.borderg {
+  border-radius: 100px;
+  border: 2px solid #cc8321;
+  letter-spacing: 1px;
+  background-size: 108% 108%;
+  -webkit-text-fill-color: white;
+  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
+  min-width: 62px !important;
+  width: 62px !important;
+  min-height: 60px !important;
+  max-height: 60px !important;
+  display: none;
+}
+.nosel.d-flex.room.r187532ce3daxff0d7ae6-x-9cyice .u-topic.dots {
+  text-transform: uppercase;
+  -webkit-text-fill-color: #e8a64d00;
+  animation: textclip 2s linear infinite;
+  display: inline-block;
+  font-size: 22px !important;
+  background-image: url(https://i.ibb.co/bB812zy/61.gif);
+  -webkit-background-clip: text;
+  color: transparent !important;
+  background-size: 100% 100%;
+  margin-top: 10px;
+  margin-right: 5px;
+  display: none;
+}
+@keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
